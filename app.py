4b020a05e54e11e5ab89a97c4f9e2ae2968bf5a6
DadeOrsu <dade.orsu@gmail.com>
2023-06-14 19:00:38 +0200

    changes of the style.css

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
-# file: app.py
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 
-@app.route('/process_form', methods=['POST'])
-def process_form():
-    name = request.form['name']
-    email = request.form['email']
+@app.route('/search_flights', methods=['POST'])
+def search_flights():
+    # Recupera i dati dal form di richiesta
+    departure_airport = request.args.get('departure_airport')
+    departure_date = request.args.get('departure_date')
+    arrival_date = request.args.get('arrival_date')
+    max_base_price = request.args.get('max_base_price')
+    max_duration = request.args.get('max_duration')
+    max_wait_time = request.args.get('max_wait_time')
+    destination = request.args.get('destination')
 
     # Esegui l'elaborazione dei dati come desiderato
 
-    return f"Name: {name}, Email: {email}"
+    return f"Departure Airport: {departure_airport}, Departure Date: {departure_date}, Arrival Date: {arrival_date}, Max Base Price: {max_base_price}, Max Duration: {max_duration}, Max Wait Time: {max_wait_time}, Destination: {destination}"
 
 
 if __name__ == '__main__':
