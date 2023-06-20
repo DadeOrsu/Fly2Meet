89f86da83bfe30d36c9b2666a0c1da8d111c1071
DadeOrsu <dade.orsu@gmail.com>
2023-06-20 17:19:06 +0200

    modifiche varie

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
+import requests
 from flask import Flask, render_template, request
 
+from helpers import get_access_token
+
 app = Flask(__name__)
+
+access_token = get_access_token('kMotx0vA8lrM8jQ0P3xZA8mAwgYMQXDS', '2JatjYoMT1mSeL0i')
+
+
+def get_data(departure_airport, _access_token):
+    url = "https://test.api.amadeus.com/v1/reference-data/locations"
+    headers = {
+        "Authorization": f"Bearer {_access_token}"
+    }
+    params = {
+        "subType": "AIRPORT",
+        "keyword": departure_airport
+    }
+    response = requests.get(url, headers=headers, params=params)
+    return response.json()
 
 
 @app.route('/search_flights', methods=['POST'])
@@ -16,10 +34,6 @@ def search_flights():
     same_airport = request.form.get('same_airport')
     one_way = request.form.get('one_way')
 
-    # Ottieni access token
-
-    # Elaborazione delle informazioni raccolte
-    # ...
     print(departure_airport, departure_date, return_date, max_base_price, max_duration, max_wait_time, destination,
           same_airport, one_way)
     return "Risultati della ricerca dei voli"
---
+++ helpers.py
@@ -0,0 +1,99 @@
+import requests
+import json
+from helpers import *
+
+
+# Funzione per ottenere i dati dei voli da file json
+def flight_from_json(file_path):
+    flights = []
+
+    try:
+        # Apri il file JSON in modalità lettura
+        with open(file_path, 'r') as file:
+            # Carica il contenuto del file JSON
+            data = json.load(file)
+
+        # Assicurati che il file contenga una lista di voli
+        if isinstance(data, list):
+            flights = data
+        else:
+            print("Il file JSON non contiene una lista di voli.")
+    except FileNotFoundError:
+        print("File non trovato.")
+    except json.JSONDecodeError:
+        print("Errore durante la decodifica del file JSON.")
+
+    return flights
+
+
+# Funzione che restituisce flight inspirations
+def get_flight_inspirations(_origin, _access_token):
+    base_url = 'https://test.api.amadeus.com/v1'
+    endpoint = '/shopping/flight-destinations'
+
+    url = base_url + endpoint
+    headers = {
+        'Authorization': f'Bearer {_access_token}'
+    }
+    params = {
+        'origin': _origin,
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']
+    else:
+        error_message = response.json()['errors'][0]['detail']
+        print(f'Errore durante la richiesta di Flight Inspirations: {error_message}')
+        return None
+
+
+# Funzione che restituisce le flight offers
+def get_flight_offers(_origin, _destination, _departure_date, _access_token):
+    url = "https://test.api.amadeus.com/v2/shopping/flight-offers"
+
+    headers = {
+        "Authorization": f"Bearer {_access_token}",
+        "Content-Type": "application/json"
+    }
+
+    params = {
+        "originLocationCode": _origin,  # Codice IATA dell'aeroporto di partenza
+        "destinationLocationCode": _destination,  # Codice IATA dell'aeroporto di destinazione
+        "departureDate": _departure_date,  # Formato: YYYY-MM-DD
+        "nonStop": "true",  # Solo voli diretti
+        "adults": 1  # Numero di adulti
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    data = response.json()
+
+    # Restituisci i dati delle offerte di volo
+    return data
+
+
+# Funzione che restituisce access token
+def get_access_token(api_key, api_secret):
+    base_url = 'https://test.api.amadeus.com'
+    endpoint = '/v1/security/oauth2/token'
+
+    url = base_url + endpoint
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    data = {
+        'grant_type': 'client_credentials',
+        'client_id': api_key,
+        'client_secret': api_secret
+    }
+
+    response = requests.post(url, headers=headers, data=data)
+
+    if response.status_code == 200:
+        token = response.json()['access_token']
+        return token
+    else:
+        print('Errore durante la richiesta di access token')
+        return None
