5d164d5546b1c59b70e64da26bc524e5ec0c7271
DadeOrsu <dade.orsu@gmail.com>
2023-06-20 16:21:37 +0200

    modifiche varie

--- app.py
+++ app.py
@@ -3,25 +3,31 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
 @app.route('/search_flights', methods=['POST'])
 def search_flights():
-    # Recupera i dati dal form di richiesta
+    # dati presi dal form
     departure_airport = request.form.get('departure_airport')
     departure_date = request.form.get('departure_date')
-    arrival_date = request.form.get('arrival_date')
+    return_date = request.form.get('return_date')
     max_base_price = request.form.get('max_base_price')
     max_duration = request.form.get('max_duration')
     max_wait_time = request.form.get('max_wait_time')
     destination = request.form.get('destination')
+    same_airport = request.form.get('same_airport')
+    one_way = request.form.get('one_way')
 
-    # Esegui l'elaborazione dei dati come desiderato
+    # Ottieni access token
 
-    return f"Departure Airport: {departure_airport}, Departure Date: {departure_date}, Arrival Date: {arrival_date}, Max Base Price: {max_base_price}, Max Duration: {max_duration}, Max Wait Time: {max_wait_time}, Destination: {destination}"
+    # Elaborazione delle informazioni raccolte
+    # ...
+    print(departure_airport, departure_date, return_date, max_base_price, max_duration, max_wait_time, destination,
+          same_airport, one_way)
+    return "Risultati della ricerca dei voli"
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
