bceb533d7d74d9cde6e20c31268848fe077894ef
DadeOrsu <dade.orsu@gmail.com>
2023-06-27 10:33:11 +0200

    introdotto un oggetto session per fare le richieste alla api

--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 import json
-from helpers import *
 
 
 # Funzione per ottenere i dati dei voli da file json
@@ -30,6 +31,23 @@ def get_flight_inspirations(_origin, _access_token):
     base_url = 'https://test.api.amadeus.com/v1'
     endpoint = '/shopping/flight-destinations'
 
+    # Crea un oggetto Session di requests
+    session = requests.Session()
+
+    # Crea un oggetto Retry per riprovare la richiesta in caso di errore
+    retries = Retry(
+        total=5,  # numero massimo di tentativi
+        backoff_factor=0.1,  # fattore di ritardo trai tentativi
+        status_forcelist=[500, 502, 503, 504]  # codici di errore per cui riprovare
+    )
+
+    # Crea un oggetto Adapter per gestire le richieste
+    adapter = HTTPAdapter(max_retries=retries)
+
+    # registra l'Adapter per la sessione
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+
     url = base_url + endpoint
     headers = {
         'Authorization': f'Bearer {_access_token}'
@@ -38,52 +56,50 @@ def get_flight_inspirations(_origin, _access_token):
         'origin': _origin,
     }
 
-    response = requests.get(url, headers=headers, params=params)
-
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']
-    else:
-        error_message = response.json()['errors'][0]['detail']
-        print(f'Errore durante la richiesta di Flight Inspirations: {error_message}')
+    try:
+        response = session.get(url, headers=headers, params=params)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']
+        else:
+            error_message = response.json()['errors'][0]['detail']
+            print(f'Errore durante la richiesta di Flight Inspirations: {error_message}')
+            return None
+    except requests.exceptions.RequestException as e:
+        print(f'Errore durante la richiesta di Flight Inspirations: {e}')
         return None
 
 
 # Funzione che restituisce le flight offers
 def get_flight_offers(_origin, _destination, _departure_date, _return_date, _max_base_price, _access_token):
     url = "https://test.api.amadeus.com/v2/shopping/flight-offers"
+    # Crea un oggetto Session di requests
+    session = requests.Session()
+
+    # Crea un oggetto Retry per riprovare la richiesta in caso di errore
+    retries = Retry(
+        total=5,  # numero massimo di tentativi
+        backoff_factor=0.1,  # fattore di ritardo trai tentativi
+        status_forcelist=[500, 502, 503, 504]  # codici di errore per cui riprovare
+    )
+
+    # Crea un oggetto Adapter per gestire le richieste
+    adapter = HTTPAdapter(max_retries=retries)
+
+    # registra l'Adapter per la sessione
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
 
     headers = {
         "Authorization": f"Bearer {_access_token}",
         "Content-Type": "application/json"
     }
-    if _return_date is not None and _max_base_price is not None:
+    if _return_date is not None:
         params = {
             "originLocationCode": _origin,  # Codice IATA dell'aeroporto di partenza
             "destinationLocationCode": _destination,  # Codice IATA dell'aeroporto di destinazione
             "departureDate": _departure_date,  # Formato: YYYY-MM-DD
             "returnDate": _return_date,  # Formato: YYYY-MM-DD
-            "maxPrice": _max_base_price,  # Prezzo massimo
-            "currencyCode": "EUR",  # Valuta
-            "nonStop": "true",  # Solo voli diretti
-            "adults": 1  # Numero di adulti
-        }
-    elif _return_date is not None:
-        print("sono qui")
-        params = {
-            "originLocationCode": _origin,  # Codice IATA dell'aeroporto di partenza
-            "destinationLocationCode": _destination,  # Codice IATA dell'aeroporto di destinazione
-            "departureDate": _departure_date,  # Formato: YYYY-MM-DD
-            "returnDate": _return_date,  # Formato: YYYY-MM-DD
-            "currencyCode": "EUR",  # Valuta
-            "nonStop": "true",  # Solo voli diretti
-            "adults": 1  # Numero di adulti
-        }
-    elif _max_base_price is not None:
-        params = {
-            "originLocationCode": _origin,  # Codice IATA dell'aeroporto di partenza
-            "destinationLocationCode": _destination,  # Codice IATA dell'aeroporto di destinazione
-            "departureDate": _departure_date,  # Formato: YYYY-MM-DD
             "maxPrice": _max_base_price,  # Prezzo massimo
             "currencyCode": "EUR",  # Valuta
             "nonStop": "true",  # Solo voli diretti
@@ -94,25 +110,46 @@ def get_flight_offers(_origin, _destination, _departure_date, _return_date, _max
             "originLocationCode": _origin,  # Codice IATA dell'aeroporto di partenza
             "destinationLocationCode": _destination,  # Codice IATA dell'aeroporto di destinazione
             "departureDate": _departure_date,  # Formato: YYYY-MM-DD
+            "maxPrice": _max_base_price,  # Prezzo massimo
             "currencyCode": "EUR",  # Valuta
             "nonStop": "true",  # Solo voli diretti
             "adults": 1  # Numero di adulti
         }
-
-    response = requests.get(url, headers=headers, params=params)
-    if response.status_code != 200:
-        print(f"Errore durante la richiesta di Flight Offers: {response.json()['errors'][0]['detail']}")
+    try:
+        response = session.get(url, headers=headers, params=params)
+        if response.status_code != 200:
+            print(f"Errore durante la richiesta di Flight Offers: {response.json()['errors'][0]['detail']}")
+            return None
+        else:
+            data = response.json()
+            # Restituisci i dati delle offerte di volo
+            return data
+    except requests.exceptions.RequestException as e:
+        print(f"Errore durante la richiesta di Flight Offers: {e}")
         return None
-    else:
-        data = response.json()
-        # Restituisci i dati delle offerte di volo
-        return data
 
 
 # Funzione che restituisce access token
 def get_access_token(api_key, api_secret):
     base_url = 'https://test.api.amadeus.com'
     endpoint = '/v1/security/oauth2/token'
+
+    # Crea un oggetto Session di requests
+    session = requests.Session()
+
+    # Crea un oggetto Retry per riprovare la richiesta in caso di errore
+    retries = Retry(
+        total=5,  # numero massimo di tentativi
+        backoff_factor=0.1,  # fattore di ritardo trai tentativi
+        status_forcelist=[500, 502, 503, 504]  # codici di errore per cui riprovare
+    )
+
+    # Crea un oggetto Adapter per gestire le richieste
+    adapter = HTTPAdapter(max_retries=retries)
+
+    # registra l'Adapter per la sessione
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
 
     url = base_url + endpoint
     headers = {
@@ -124,11 +161,14 @@ def get_access_token(api_key, api_secret):
         'client_secret': api_secret
     }
 
-    response = requests.post(url, headers=headers, data=data)
-
-    if response.status_code == 200:
-        token = response.json()['access_token']
-        return token
-    else:
-        print('Errore durante la richiesta di access token')
+    try:
+        response = session.post(url, headers=headers, data=data)
+        if response.status_code == 200:
+            token = response.json()['access_token']
+            return token
+        else:
+            print('Errore durante la richiesta di access token')
+            return None
+    except requests.exceptions.RequestException as e:
+        print(f'Errore durante la richiesta di access token: {e}')
         return None
