6a0ff89481beb832e98e7c7102cba1dbc7bf7abe
DadeOrsu <dade.orsu@gmail.com>
2023-07-17 09:44:51 +0200

    fly2meet.pl adesso consulta i fatti contenuti in prologFacts/prolog_facts.pl

--- fly2meet.pl
+++ fly2meet.pl
@@ -1,25 +1,4 @@
-airport(lin).
-airport(cdg).
-airport(lgw).
-airport(lcy).
-airport(ory).
-airport(lhr).
-flight(ory, lin, az, 357, date(2023, 7, 27, 7, 30, 0, 3600, cest, true), date(2023, 7, 27, 8, 55, 0, 3600, cest, true), 5100, 159.43).
-flight(ory, lin, az, 351, date(2023, 7, 27, 9, 10, 0, 3600, cest, true), date(2023, 7, 27, 10, 35, 0, 3600, cest, true), 5100, 159.43).
-flight(cdg, lin, az, 305, date(2023, 7, 27, 9, 20, 0, 3600, cest, true), date(2023, 7, 27, 10, 50, 0, 3600, cest, true), 5400, 160.59).
-flight(cdg, lin, az, 313, date(2023, 7, 27, 16, 55, 0, 3600, cest, true), date(2023, 7, 27, 18, 25, 0, 3600, cest, true), 5400, 188.59).
-flight(cdg, lin, az, 315, date(2023, 7, 27, 19, 25, 0, 3600, cest, true), date(2023, 7, 27, 20, 55, 0, 3600, cest, true), 5400, 188.59).
-flight(cdg, lin, az, 311, date(2023, 7, 27, 21, 30, 0, 3600, cest, true), date(2023, 7, 27, 23, 0, 0, 3600, cest, true), 5400, 188.59).
-flight(cdg, lin, af, 1312, date(2023, 7, 27, 18, 20, 0, 3600, cest, true), date(2023, 7, 27, 19, 45, 0, 3600, cest, true), 5100, 193.87).
-flight(cdg, lin, af, 1212, date(2023, 7, 27, 7, 20, 0, 3600, cest, true), date(2023, 7, 27, 8, 50, 0, 3600, cest, true), 5400, 193.87).
-flight(cdg, lhr, af, 1180, date(2023, 7, 27, 18, 15, 0, 3600, cest, true), date(2023, 7, 27, 18, 35, 0, 0, bst, true), 4800, 137.87).
-flight(cdg, lhr, af, 1280, date(2023, 7, 27, 16, 10, 0, 3600, cest, true), date(2023, 7, 27, 16, 35, 0, 0, bst, true), 5100, 137.87).
-flight(cdg, lhr, af, 1380, date(2023, 7, 27, 20, 55, 0, 3600, cest, true), date(2023, 7, 27, 21, 20, 0, 0, bst, true), 5100, 137.87).
-flight(cdg, lhr, af, 1580, date(2023, 7, 27, 10, 0, 0, 3600, cest, true), date(2023, 7, 27, 10, 30, 0, 0, bst, true), 5400, 137.87).
-flight(cdg, lhr, af, 1680, date(2023, 7, 27, 7, 35, 0, 3600, cest, true), date(2023, 7, 27, 8, 0, 0, 0, bst, true), 5100, 137.87).
-flight(ory, lgw, vy, 8942, date(2023, 7, 27, 6, 55, 0, 3600, cest, true), date(2023, 7, 27, 7, 0, 0, 0, bst, true), 3900, 148.43).
-
-
+:- consult('prologFacts/prolog_facts.pl').
 
 fly2Meet(Airport1, Airport2, SortStrategy, Flights) :-
     airport(Airport1), airport(Airport2), dif(Airport1, Airport2),
