f25019a69d2d61ea29252d43b7da6816a0764599
DadeOrsu <dade.orsu@gmail.com>
2023-09-14 22:58:23 +0200

    modificati prolog unit tests.

--- test_fly2meet.pl
+++ test_fly2meet.pl
@@ -22,66 +22,99 @@ teardown2 :-
 
 test(fly2meet_case1, [setup(setup1), cleanup(teardown1)]) :-
     once(fly2meet(cdg, lhr, bestsolution, no, 4000, yes, yes, Flights)),
-    assertion(nonvar(Flights)).
+    assertion(is_list(Flights)),
+    sort(4, @=<, Flights, SortedFlights),
+    assertion(Flights = SortedFlights).
 
 test(fly2meet_case2, [setup(setup1), cleanup(teardown1)]) :-
     once(fly2meet(cdg, lhr, bestsolution, no, 4000, yes, no, Flights)),
-    assertion(nonvar(Flights)).
+    assertion(is_list(Flights)),
+    sort(4, @=<, Flights, SortedFlights),
+    assertion(Flights = SortedFlights).
 
 test(fly2meet_case3, [setup(setup1), cleanup(teardown1)]) :-
     once(fly2meet(cdg, lhr, bestsolution, no, 4000, no, yes, Flights)),
-    assertion(nonvar(Flights)).
+    assertion(is_list(Flights)),
+    sort(4, @=<, Flights, SortedFlights),
+    assertion(Flights = SortedFlights).
 
 test(fly2meet_case4, [setup(setup1), cleanup(teardown1)]) :-
     once(fly2meet(cdg, lhr, bestsolution, no, 4000, no, no, Flights)),
-    assertion(nonvar(Flights)).
+    assertion(is_list(Flights)),
+    sort(4, @=<, Flights, SortedFlights),
+    assertion(Flights = SortedFlights).
 
 test(fly2meet_case5, [setup(setup2), cleanup(teardown2)]) :-
     once(fly2meet(cdg, lhr, bestsolution, yes, 4000, yes, yes, Flights)),
-    assertion(nonvar(Flights)).
+    assertion(is_list(Flights)),
+    sort(4, @=<, Flights, SortedFlights),
+    assertion(Flights = SortedFlights).
 
 test(fly2meet_case6, [setup(setup2), cleanup(teardown2)]) :-
     once(fly2meet(cdg, lhr, bestsolution, yes, 4000, yes, no, Flights)),
-    assertion(nonvar(Flights)).
+    assertion(is_list(Flights)),
+    sort(4, @=<, Flights, SortedFlights),
+    assertion(Flights = SortedFlights).
 
 test(fly2meet_case7, [setup(setup2), cleanup(teardown2)]) :-
     once(fly2meet(cdg, lhr, bestsolution, yes, 4000, no, yes, Flights)),
-    assertion(nonvar(Flights)).
+    assertion(is_list(Flights)),
+    sort(4, @=<, Flights, SortedFlights),
+    assertion(Flights = SortedFlights).
 
 test(fly2meet_case8, [setup(setup2), cleanup(teardown2)]) :-
     once(fly2meet(cdg, lhr, bestsolution, yes, 4000, no, no, Flights)),
-    assertion(nonvar(Flights)).
+    assertion(is_list(Flights)),
+    sort(4, @=<, Flights, SortedFlights),
+    assertion(Flights = SortedFlights).
+
 test(fly2meet_case1, [setup(setup1), cleanup(teardown1)]) :-
     once(fly2meet(cdg, lhr, bestsolution, no, inf, yes, yes, Flights)),
-    assertion(nonvar(Flights)).
+    assertion(is_list(Flights)),
+    sort(4, @=<, Flights, SortedFlights),
+    assertion(Flights = SortedFlights).
 
 test(fly2meet_case2, [setup(setup1), cleanup(teardown1)]) :-
     once(fly2meet(cdg, lhr, bestsolution, no, inf, yes, no, Flights)),
-    assertion(nonvar(Flights)).
+    assertion(is_list(Flights)),
+    sort(4, @=<, Flights, SortedFlights),
+    assertion(Flights = SortedFlights).
 
 test(fly2meet_case3, [setup(setup1), cleanup(teardown1)]) :-
     once(fly2meet(cdg, lhr, bestsolution, no, inf, no, yes, Flights)),
-    assertion(nonvar(Flights)).
+    assertion(is_list(Flights)),
+    sort(4, @=<, Flights, SortedFlights),
+    assertion(Flights = SortedFlights).
 
 test(fly2meet_case4, [setup(setup1), cleanup(teardown1)]) :-
     once(fly2meet(cdg, lhr, bestsolution, no, inf, no, no, Flights)),
-    assertion(nonvar(Flights)).
+    assertion(is_list(Flights)),
+    sort(4, @=<, Flights, SortedFlights),
+    assertion(Flights = SortedFlights).
 
 test(fly2meet_case5, [setup(setup2), cleanup(teardown2)]) :-
     once(fly2meet(cdg, lhr, bestsolution, yes, inf, yes, yes, Flights)),
-    assertion(nonvar(Flights)).
+    assertion(is_list(Flights)),
+    sort(4, @=<, Flights, SortedFlights),
+    assertion(Flights = SortedFlights).
 
 test(fly2meet_case6, [setup(setup2), cleanup(teardown2)]) :-
     once(fly2meet(cdg, lhr, bestsolution, yes, inf, yes, no, Flights)),
-    assertion(nonvar(Flights)).
+    assertion(is_list(Flights)),
+    sort(4, @=<, Flights, SortedFlights),
+    assertion(Flights = SortedFlights).
 
 test(fly2meet_case7, [setup(setup2), cleanup(teardown2)]) :-
     once(fly2meet(cdg, lhr, bestsolution, yes, inf, no, yes, Flights)),
-    assertion(nonvar(Flights)).
+    assertion(is_list(Flights)),
+    sort(4, @=<, Flights, SortedFlights),
+    assertion(Flights = SortedFlights).
 
 test(fly2meet_case8, [setup(setup2), cleanup(teardown2)]) :-
     once(fly2meet(cdg, lhr, bestsolution, yes, inf, no, no, Flights)),
-    assertion(nonvar(Flights)).
+    assertion(is_list(Flights)),
+    sort(4, @=<, Flights, SortedFlights),
+    assertion(Flights = SortedFlights).
 
 :- end_tests(fly2meet).
 
